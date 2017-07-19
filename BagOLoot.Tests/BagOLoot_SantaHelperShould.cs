49dba50c5f217e230a85301390c3167645c72126
Joey Biotti <joey@MacBook-Pro.local>
2017-07-19 10:16:39 -0500

    add bag test works

---
+++ BagOLoot.Tests/BagOLoot_SantaHelperShould.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BagOLoot.Tests
+{
+    public class SantaHelperShould
+    {
+        SantaHelper _helper = new SantaHelper();
+        public SantaHelperShould()
+        {
+            _helper = new SantaHelper();
+        }
+
+        [Fact]
+        public void AddToyToChildsBag()
+        {
+            string toyName = "Skateboard";
+            int childId = 715;
+            int toyId = _helper.AddToyToBag(toyName, childId);
+            List <int> toys = _helper.GetChildsToys(childId);
+
+            Assert.Contains(toyId, toys);
+        }
+    }
+}
