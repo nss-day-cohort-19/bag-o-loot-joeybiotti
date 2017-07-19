8a186ff19df0beb2f1a19adcb0a4b8a6db5a31ff
Joey Biotti <joey@MacBook-Pro.local>
2017-07-19 14:45:33 -0500

    all tests pass

--- BagOLoot/ChildRegister.cs
+++ BagOLoot/ChildRegister.cs
@@ -50,7 +50,23 @@ namespace BagOLoot
 
         public List<string> GetChildren ()
         {
-            return new List<string>();
+            using (_connection)
+            {
+                _connection.Open();
+                SqliteCommand dbcmd = _connection.CreateCommand();
+                dbcmd.CommandText = "select id, name from child";
+
+                using (SqliteDataReader dr = dbcmd.ExecuteReader())
+                {
+                    while(dr.Read())
+                    {
+                        _children.Add(dr[1].ToString());
+                    }
+                }
+                dbcmd.Dispose();
+                _connection.Close();
+            }
+            return _children;
         }
 
         public string GetChild (string name)
--- BagOLoot/Santa.cs
+++ BagOLoot/Santa.cs
@@ -7,7 +7,10 @@ namespace BagOLoot
         public List<int> CheckSantasList(int child)
         {
             return new List<int>(){1,2,3};
-        
+        }
+        public List <int> RunYuletideReport(int childId, int toyId)
+        {
+            return new List<int>(){1,2,323,2323};
         }
     }
 }
