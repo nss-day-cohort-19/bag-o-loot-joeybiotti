03c87d50480044129ed505b1ffd9952c3ad30b66
Joey Biotti <joey@MacBook-Pro.local>
2017-07-21 10:18:03 -0500

    added sql connection

--- BagOLoot/Santa.cs
+++ BagOLoot/Santa.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Linq; 
+using Microsoft.Data.Sqlite;
 
 namespace BagOLoot
 {
     public class SantaClaus
     {
+        private List<string> _santa = new List<string>();
+        private string _connectionString = $"Data Source={Environment.GetEnvironmentVariable("BAGOLOOT_DB")}";
+        private SqliteConnection _connection; 
         public List<int> CheckSantasList(int child)
         {
             return new List<int>(){1,2,3};
